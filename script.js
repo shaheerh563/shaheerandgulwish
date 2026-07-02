82aa87c8e3982e00e5e52f8826aba5fa983d61c4
shaheerh563 <shaheerh563@gmail.com>
2026-07-01 23:49:06 -0400

    Use updated Nikkah card asset

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const EVENTS = {
     city: "Toronto",
     date: "August 22, 2026",
     summary: "Toronto Nikkah, August 22, 2026 at 5:00 PM InshaAllah",
-    image: "/shaheerandgulwish/nikkah-card.jpeg?v=20260627h",
+    image: "/shaheerandgulwish/nikkah-card-20260701.svg",
     imageAlt: "Toronto nikkah invitation card"
   },
   walima: {
